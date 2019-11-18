470c144cfef26d901750c791743ae17d82dfd20f
BenHall-7 <benjaminjahall@gmail.com>
2019-11-17 23:37:54 -0500

    Page content displaying

--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -2,8 +2,8 @@ import React from "react";
 
 export const NumberButton = props => {
   return (
-    <>
-      {/* Display a button element rendering the data being passed down from the parent container on props */}
-    </>
+    <button>
+      {props.num}
+    </button>
   );
 };
--- src/components/ButtonComponents/SpecialButtons/SpecialButton.js
+++ src/components/ButtonComponents/SpecialButtons/SpecialButton.js
@@ -1,9 +1,9 @@
 import React from "react";
 
-export const SpecialButton = () => {
+export const SpecialButton = props => {
   return (
-    <>
-      {/* Display a button element rendering the data being passed down from the parent container on props */}
-    </>
+    <button>
+      {props.sp}
+    </button>
   );
 };
